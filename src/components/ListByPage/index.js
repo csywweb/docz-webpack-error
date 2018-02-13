fa59aac8d82c5aab0ba865011f074a95773312ed
zhouzhen <zhouzhen@youzan.com>
2018-02-13 14:18:53 +0800

    feat(all):fitst commit

---
+++ src/components/ListByPage/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+    Table,
+} from 'zent';
+import mapKeys from 'lodash/mapKeys';
+import isEqual from 'lodash/isEqual';
+import assign from 'object-assign';
+
+/**
+ * filter   筛选参数
+ * total
+ * list     列表数据
+ * */
+
+export default class ListByPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: props.loading,
+            page: {
+                current: 1,
+            },
+            filter: props.filter,
+            total: 0,
+            list: props.list,
+        };
+    }
+
+    static defaultProps = {
+        filter: {},
+        list: [],
+        pageKey: 'pageNo',
+        pageSize: 20,
+        pagination: true,
+        columns: [],
+        className: '',
+        loading: false,
+    };
+
+
+    static propTypes = {
+        action: PropTypes.func.isRequired,
+    };
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!isEqual(this.state.filter, nextProps.filter)) {
+            this.setState({
+                filter: nextProps.filter,
+                page: { current: 1 },
+            }, () => {
+                this.fetchData();
+            });
+        } else {
+            this.setState({
+                total: nextProps.total,
+                list: nextProps.list,
+            });
+        }
+    }
+
+    getPageData(page) {
+        this.setState({ page }, () => {
+            this.fetchData();
+        });
+    }
+
+    fetchData = () => {
+        this.setState({ loading: true });
+        return this.props.action(assign({}, this.state.filter, mapKeys(this.state.page, () => this.props.pageKey))).then(() => {
+            this.setState({ loading: false });
+        }).catch((err) => {
+            this.setState({ loading: false });
+        });
+    };
+
+
+    render() {
+        const {
+            pageSize,
+            pagination,
+        } = this.props;
+
+        const {
+            loading,
+            total,
+            list,
+            page,
+        } = this.state;
+
+        const pageInfo = assign({
+            limit: pageSize,
+            total,
+        }, page);
+
+        return (
+            <Table
+                {...this.props}
+                datasets={list}
+                loading={loading}
+                pageInfo={pagination ? pageInfo : null}
+                onChange={this.getPageData.bind(this)}
+            />
+        );
+    }
+}
