74a1e4fb39f17c915541f67b1c0d89f065576496
zhouzhen <zhouzhen@youzan.com>
2018-12-12 16:43:46 +0800

    install css 插件

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const __global = require('./__global');
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
