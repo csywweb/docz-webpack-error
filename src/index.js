d15ad7a66e322ed8e5a0e71f23b38f6abb32fd6a
abczhijia <abczhijia@youzan.com>
2018-12-28 16:00:49 +0800

    beauty_table

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // 表单
 import AddressField from './components/AddressField';
 // 表格
-import BeautyTable from './components/BeautyTable';
+import BeautyTable from './components/beauty_table';
 import ListByPage from './components/ListByPage';
 import SelectDept from './components/select_dept';
 import SimpleForm from './components/simple_form';
