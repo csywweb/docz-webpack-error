c983c17909f7f2dad764b91a22ddad0accf6f6b0
caosiyuan <caosiyuan@youzan.com>
2019-10-07 21:32:00 +0800

    refactor(docz): 新增docz替换storybook

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+];
+const plugins = [
+    'add-module-exports',
+];
+
+module.exports = {
+    presets,
+    plugins,
+};
---
+++ doczrc.js
@@ -0,0 +1,22 @@
+import { createPlugin } from 'docz-core';
+
+const myPlugin = () => createPlugin({
+    onCreateBabelConfig({ actions }) {
+        actions.setBabelPlugin({
+            name: 'babel-plugin-zent',
+            options: { automaticStyleImport: true },
+        });
+    },
+});
+
+
+export default {
+    title: '美业 react 组件库',
+
+    description: '美业 react 组件库',
+
+    menu: ['开始', '通用组件'],
+    ignore: ['README.md', 'CHANGELOG.md', 'README.md'],
+
+    plugins: [myPlugin()],
+};
